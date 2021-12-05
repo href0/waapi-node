73d2e689e1052cf6d6ce6b2fe3563d9ecf5a2d7d
hrefdev <href.dev@gmail.com>
2021-12-05 12:42:35 +0700

    add postgres for session

--- app.js
+++ app.js
@@ -1,152 +1,143 @@
-const {
-    Client
-} = require('whatsapp-web.js');
-const express = require('express');
-const socketIO = require('socket.io')
-const qrcode = require('qrcode');
-const fs = require('fs');
-const http = require('http')
-const {
-    phoneNumberFormatter
-} = require('./helper/formatter');
-const {
-    check,
-    validationResult
-} = require('express-validator');
+const { Client } = require("whatsapp-web.js");
+const express = require("express");
+const socketIO = require("socket.io");
+const qrcode = require("qrcode");
+const fs = require("fs");
+const http = require("http");
+const { phoneNumberFormatter } = require("./helper/formatter");
+const { check, validationResult } = require("express-validator");
 
 const app = express();
-const server = http.createServer(app)
-const io = socketIO(server)
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = process.env.PORT || 8000;
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
+// const SESSION_FILE_PATH = "./whatsapp-session.json";
+// let sessionCfg;
+// if (fs.existsSync(SESSION_FILE_PATH)) {
+//   sessionCfg = require(SESSION_FILE_PATH);
+// }
+const db = require("./helper/db.js");
+(async () => {
+  app.get("/", (req, res) => {
+    res.sendFile("index.html", {
+      root: __dirname,
+    });
+  });
+  const savedSession = await db.readSession();
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {
-        root: __dirname
-    })
-})
-const SESSION_FILE_PATH = './whatsapp-session.json';
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
-
-const createSession = () => {
+  //socket io
+  io.on("connection", (socket) => {
     const client = new Client({
-        restartOnAuthFail: true,
-        puppeteer: {
-            headless: true,
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-accelerated-2d-canvas',
-                '--no-first-run',
-                '--no-zygote',
-                '--single-process', // <- this one doesn't works in Windows
-                '--disable-gpu'
-            ],
-        },
-        session: sessionCfg
+      restartOnAuthFail: true,
+      puppeteer: {
+        headless: true,
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+          "--disable-accelerated-2d-canvas",
+          "--no-first-run",
+          "--no-zygote",
+          "--single-process", // <- this one doesn't works in Windows
+          "--disable-gpu",
+        ],
+      },
+      session: savedSession,
     });
-    client.on('message', msg => {
-        if (msg.body == 'p' || msg.body == 'P') {
-            msg.reply('papope');
-        }
+    client.on("message", (msg) => {
+      if (msg.body == "p" || msg.body == "P") {
+        msg.reply("papope");
+      }
     });
-    client.on('disconnected', (reason) => {
-        console.log('Whatsapp disconnected')
-        fs.unlinkSync(SESSION_FILE_PATH, function (err) {
-            if (err) return console.log(err);
-            console.log('Session file deleted!');
-        });
-        client.destroy();
-        client.initialize();
-    });
-
     client.initialize();
+    socket.emit("message", "Connecting... please wait");
 
-    client.on('qr', (qr) => {
-        // Generate and scan this code with your phone
-        console.log('QR RECEIVED', qr);
-        qrcode.toDataURL(qr, (errm, url) => {
-            io.emit('qr', url)
-            io.emit('message', 'QR Code received, please scan')
-            io.emit('scan', 'QR Code received, please scan')
-        });
+    client.on("disconnected", (reason) => {
+      console.log("Whatsapp disconnected");
+      db.removeSession();
+      client.destroy();
+      client.initialize();
     });
-    client.on('ready', () => {
-        console.log('ready')
-        io.emit('message', 'Whatsapp Ready')
-        io.emit('ready', 'Whatsapp Ready')
-        // if (fs.existsSync(SESSION_FILE_PATH)) {
-        //     io.emit('ready', 'Whatsapp Ready')
-        //     io.emit('authenticated', 'Whatsapp authenticated')
-        // }
-    });
-    client.on('authenticated', (session) => {
-        console.log('AUTHENTICATED', session);
-        io.emit('authenticated', 'Whatsapp authenticated')
-        io.emit('message', 'Whatsapp authenticated')
-        sessionCfg = session;
-        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-            if (err) {
-                console.error(err);
-            }
-        });
-    });
-    client.on('auth_failure', function (session) {
-        io.emit('message', 'Auth failure, restarting...');
-    });
-}
 
-//socket io
-io.on('connection', (socket) => {
-    socket.emit('message', 'Connecting... please wait');
-    createSession()
-})
-
-// send message
-app.post('/send-message', [
-    check('number')
-    .isLength({
-        min: 5
-    })
-    .withMessage('must be at least 5 chars long')
-    .matches(/^[0-9]+$/)
-    .withMessage('number only'),
-    check('message').notEmpty().withMessage('message cannot be empty'),
-], (req, res) => {
-    const errors = validationResult(req).formatWith(({
-        msg
-    }) => {
-        return msg;
+    client.on("qr", (qr) => {
+      // Generate and scan this code with your phone
+      console.log("QR RECEIVED", qr);
+      qrcode.toDataURL(qr, (errm, url) => {
+        socket.emit("qr", url);
+        socket.emit("message", "QR Code received, please scan");
+        socket.emit("scan", "QR Code received, please scan");
+      });
     });
-    if (!errors.isEmpty()) {
-        return res.status(422).json({
-            status: false,
-            message: errors.mapped()
+    client.on("ready", () => {
+      console.log("ready");
+      socket.emit("message", "Whatsapp Ready");
+      socket.emit("ready", "Whatsapp Ready");
+      // if (fs.existsSync(SESSION_FILE_PATH)) {
+      //     io.emit('ready', 'Whatsapp Ready')
+      //     io.emit('authenticated', 'Whatsapp authenticated')
+      // }
+    });
+    client.on("authenticated", (session) => {
+      console.log("AUTHENTICATED", session);
+      socket.emit("authenticated", "Whatsapp authenticated");
+      socket.emit("message", "Whatsapp authenticated");
+      db.saveSession(session);
+    });
+    client.on("auth_failure", function (session) {
+      socket.emit("message", "Auth failure, restarting...");
+    });
+  });
+
+  // send message
+  app.post(
+    "/send-message",
+    [
+      check("number")
+        .isLength({
+          min: 5,
         })
-    }
-    const number = phoneNumberFormatter(req.body.number);
-    const message = req.body.message;
-    client.sendMessage(number, message).then(response => {
-        res.status(200).json({
+        .withMessage("must be at least 5 chars long")
+        .matches(/^[0-9]+$/)
+        .withMessage("number only"),
+      check("message").notEmpty().withMessage("message cannot be empty"),
+    ],
+    (req, res) => {
+      const errors = validationResult(req).formatWith(({ msg }) => {
+        return msg;
+      });
+      if (!errors.isEmpty()) {
+        return res.status(422).json({
+          status: false,
+          message: errors.mapped(),
+        });
+      }
+      const number = phoneNumberFormatter(req.body.number);
+      const message = req.body.message;
+      client
+        .sendMessage(number, message)
+        .then((response) => {
+          res.status(200).json({
             status: true,
-            response: response
-        });
-    }).catch(err => {
-        res.status(500).json({
+            response: response,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({
             status: false,
-            response: err
+            response: err,
+          });
         });
-    });
-});
+    }
+  );
 
-server.listen(port, () => {
-    console.log('app running at:' + port);
-});
+  server.listen(port, () => {
+    console.log("app running at:" + port);
+  });
+})();
---
+++ helper/db.js
@@ -0,0 +1,53 @@
+const { Client } = require("pg");
+
+// const url = "postgres://xemomvtgpexztz:71ad1d6161097988964ea001daa146384e8d7181d72bbf173c8139f3fbfc0ed1@ec2-44-193-111-218.compute-1.amazonaws.com:5432/d4541ko6l6l8ub"
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+const readSession = async () => {
+  try {
+    const res = await client.query(
+      "SELECT * FROM wa_session ORDER BY created_at DESC LIMIT 1"
+    );
+    if (res.rows.length) return res.rows[0].session;
+    return "";
+  } catch (error) {
+    throw error;
+  }
+};
+
+const saveSession = async (session) => {
+  client.query(
+    "INSERT INTO wa_session (session) VALUES($1)",
+    [session],
+    (err, results) => {
+      if (err) {
+        console.error("Failed to save session" + err);
+      } else {
+        console.log("session saved");
+      }
+    }
+  );
+};
+
+const removeSession = async () => {
+  client.query("DELETE FROM wa_session", (err, results) => {
+    if (err) {
+      console.error("failed to remove session : " + err);
+    } else {
+      console.log("session deleted");
+    }
+  });
+};
+
+module.exports = {
+  readSession,
+  saveSession,
+  removeSession,
+};
